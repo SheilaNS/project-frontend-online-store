118b41c90b674523fff06a06a0470188a9f2de8a
sorinesavage <pvmg1@hotmail.com>
2022-03-10 18:05:55 -0300

    Finalizados Requisitos 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import Routes from './Routes'
+import Home from './components/Home';
 
 class App extends React.Component {
   render() {
     return (
-      <div>Oie</div>  
+      <Routes />
     );
   }
 }
